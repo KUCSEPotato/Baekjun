a186bdf529c38b51ac146c794f561309a422c020
KUCSEPotato <a23493307@gmail.com>
2025-11-29 12:34:39 +0900

    solve ./prob1193_분수 찾기.py

--- Silver/prob1193_분수 찾기.py
+++ Silver/prob1193_분수 찾기.py
@@ -13,5 +13,42 @@
 # 첫째 줄에 분수를 출력한다.
 # ---------------------------------------------------------------------------------------------------- #
 
-def matrix(a):
+# 중요한 규칙
+# 1. n번째 대각선 시점에서는 총 n(n+1)/2 개의 원소가 있다.
+# 2. n번째 대각선에서 -> n이 짝수면 1/n에서 n/1로, n이 홀수면 n/1에서 1/n으로 간다.
+
+# 문제 풀이 전략
+# 1. 몇 번째 대각선인지 구한다.
+# 2. n번째 대각선에서 몇번째인지 구한다.
+
+# n(n+1)//2 < X 
+def find_diagonal(X:int) -> int:
+    n = 1
+    while X > n*(n+1)//2:
+        n+=1
+    return n
+
+def get_fraction(X: int):
+    diagonal = find_diagonal(X=X)
+    prev = diagonal*(diagonal-1)//2 # 이전 대각선 까지의 원소 개수
+    offset = X - prev - 1 # zero-based offset
+
+    if diagonal % 2 == 0:
+        # 짝수 대각선: 1/n -> n/1
+        child = 1 + offset
+        parent = diagonal - offset
+    else:
+        # 홀수 대각선: n/1 -> 1/n
+        child = diagonal - offset
+        parent = 1 + offset
+    return child, parent
+
+def main():
+    X = int(input())
+    child, parent = get_fraction(X)
+    print(f"{child}/{parent}")
+
+if __name__ == "__main__":
+    main()
+
     
